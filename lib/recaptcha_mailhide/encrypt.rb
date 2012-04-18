c200e37512c6a3997b31692beb0c875d77b02cd9
Pilaf <pfayolle@gmail.com>
2012-04-18 11:25:12 -0300

    Fixed bug with long email addresses.

--- lib/recaptcha_mailhide/encrypt.rb
+++ lib/recaptcha_mailhide/encrypt.rb
@@ -6,33 +6,51 @@ module RecaptchaMailhide
     BLOCK_SIZE = 16
     INITIALIZATION_VECTOR = 0.chr * 16
 
+    # Encrpyts the given string using AES 128 and
+    # returns the result in URL-safe Base64 form.
+    #
     def self.encrypt(string)
       aes = build_aes
-      encrypted = aes.update(pad_string(string)) + aes.final
-      urlsafe_base64(encrypted)
+      urlsafe_base64(aes.update(pad_string(string)) + aes.final)
     end
 
   private
 
+    # Converts the given string to Base64 encoding,
+    # replacing '+' with '-', '/' with '_' and
+    # removing newlines in order to make it URL-safe.
+    #
     def self.urlsafe_base64(string)
-      Base64.encode64(string).tr('+/', '-_').chomp
+      Base64.encode64(string).tr('+/', '-_').gsub("\n", '')
     end
 
+    # Pads the given string to blocks of 16 bytes
+    # using bytes representing the number of bytes
+    # used for padding.
+    #
     def self.pad_string(string)
       padding = BLOCK_SIZE - (string.length % BLOCK_SIZE)
       string + padding.chr * padding
     end
 
+    # Initializes the cipher.
+    #
     def self.build_aes
       aes = OpenSSL::Cipher::Cipher.new("aes-128-cbc")
       aes.encrypt
       aes.iv = INITIALIZATION_VECTOR
       aes.padding = 0
 
-      # Convert the private key from hex to binary
-      aes.key = RecaptchaMailhide.configuration.private_key.unpack('a2' * 16).map {|x| x.hex }.pack('c' * 16)
+      aes.key = binary_public_key
 
       aes
+    end
+
+    # Returns the private key converted from hex
+    # to binary.
+    #
+    def self.binary_public_key
+      RecaptchaMailhide.configuration.private_key.unpack('a2' * 16).map {|x| x.hex }.pack('c' * 16)
     end
   end
 end
--- spec/recaptcha_mailhide/encrypt_spec.rb
+++ spec/recaptcha_mailhide/encrypt_spec.rb
@@ -9,5 +9,9 @@ describe RecaptchaMailhide::Encrypt do
     it("properly encrypts johndoe@example.com") do
       RecaptchaMailhide::Encrypt.encrypt("johndoe@example.com").should eq("whWIqk0r4urZ-3S7y7uSceC9_ECd3hpAGy71E2o0HpI=")
     end
+
+    it("properly encrypts long emails") do
+      RecaptchaMailhide::Encrypt.encrypt("this.sure.is.a.very.long.email.address@example.com").should eq("Ax_miTfkXPP_jPPoe130TlwgEaiBCPUuCxaJy3KalZsWP8_Z_Qcc1Awpwk-cPjVvnsbi6P6Bf_de2U1A5aocVA==")
+    end
   end
 end
