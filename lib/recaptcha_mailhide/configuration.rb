21e5beee94ad077072a080efd4e14346a2d77b79
Pilaf <pfayolle@gmail.com>
2015-11-11 01:22:27 -0300

    Make private_key and public_key raise exceptions if missing from config.

--- lib/recaptcha_mailhide/configuration.rb
+++ lib/recaptcha_mailhide/configuration.rb
@@ -1,5 +1,15 @@
 module RecaptchaMailhide
   class Configuration
-    attr_accessor :private_key, :public_key
+    attr_writer :private_key, :public_key
+
+    def private_key
+      raise "RecaptchaMailhide's private_key is not set. If you're using Rails add an initializer to config/initializers." unless @private_key
+      @private_key
+    end
+
+    def public_key
+      raise "RecaptchaMailhide's public_key is not set. If you're using Rails add an initializer to config/initializers." unless @public_key
+      @public_key
+    end
   end
 end
