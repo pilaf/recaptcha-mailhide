4299f922c6b19937504b9720b81ee5fab9617977
Pilaf <pfayolle@gmail.com>
2015-11-11 01:22:59 -0300

    Use HTTPS in generated URLs.

--- lib/recaptcha_mailhide/url.rb
+++ lib/recaptcha_mailhide/url.rb
@@ -1,11 +1,11 @@
 module RecaptchaMailhide
   module URL
-    BASE_URL = "http://www.google.com/recaptcha/mailhide/d"
+    BASE_URL = "https://www.google.com/recaptcha/mailhide/d"
 
-    def self.url_for(email)
+    def self.url_for(email_address)
       public_key = RecaptchaMailhide.configuration.public_key
-      encrypted_email = RecaptchaMailhide::Encrypt.encrypt(email)
-      BASE_URL + "?k=#{public_key}&c=#{encrypted_email}"
+      encrypted_address = Encrypt.encrypt(email_address)
+      BASE_URL + "?k=#{public_key}&c=#{encrypted_address}"
     end
   end
 end
